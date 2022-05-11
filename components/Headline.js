afde932685933a72968c0a583125040fb45bb373
shamo-9118 <won9118391183@gmail.com>
2022-05-11 20:34:43 +0900

    ♻️ 各コンポーネントの作成

--- components/Headline.js
+++ components/Headline.js
@@ -4,11 +4,11 @@ export function Headline(props) {
   return (
     <div> 
      <h1 className={styles.title}>
-      {props.title} page
+       {props.page} page
      </h1>
      <p className={styles.description}>
        Get started by editing{' '}
-       <code className={styles.code}>pages/index.js</code>
+       {props.children}
      </p>
     </div>
   )
