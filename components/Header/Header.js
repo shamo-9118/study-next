60e5ad91b210984525199037ed52c49a39545f07
shamo-9118 <won9118391183@gmail.com>
2022-05-12 09:01:04 +0900

    ♻️ cssを追加

---
+++ components/Header/Header.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import classes from "../Header/Header.module.css"
+
+export function Header() {
+  return (
+      <header className={classes.header}>
+        <Link href="/"><a  className={ classes.anchor}>Index</a></Link>
+        <Link href="/about"><a  className={ classes.anchor}>About</a></Link>
+      </header>
+  )
+}
